6b6584494dda55f86b8c4a6d1385e3f8e473aede
Suyi Diao <suyi.diao@vanderbilt.edu>
2017-05-28 23:06:58 -0500

    added consumable

---
+++ Assets/Script/ConsumableSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ConsumableSpawner : NetworkBehaviour
+{
+	public Consume ConsumablePrefab;
+	public GameObject ConsumableHolder;
+	public int maxConsumableNum;
+	public GameObject RightEdge;
+	public GameObject UpEdge;
+
+	public void Spawn ()
+	{
+		Vector3 randompos = new Vector3 ((Random.value * 2 - 1) * RightEdge.transform.position.x, (Random.value * 2 - 1) * UpEdge.transform.position.y);
+		// Spawn the consumable
+		Consume newConsumable;
+		newConsumable = (Consume)Instantiate (ConsumablePrefab, randompos, Quaternion.identity);
+		newConsumable.onDied += OnConsDied;
+		newConsumable.gameObject.transform.parent = ConsumableHolder.transform;
+		NetworkServer.Spawn (newConsumable.gameObject);
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < maxConsumableNum; i++) {
+			Spawn ();
+		}
+	}
+
+	public void OnConsDied ()
+	{
+		Spawn ();
+	}
+
+}
---
+++ Assets/Script/Consume.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Consume : NetworkBehaviour
+{
+	public delegate void OnDied ();
+
+	public event OnDied onDied;
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<SpriteRenderer> ().color = Random.ColorHSV (0f, 1f, 1f, 1f, 0.5f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (Vector3.forward * 100 * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			if (onDied != null)
+				onDied ();
+			NetworkServer.Destroy (gameObject);
+		}
+	}
+}
